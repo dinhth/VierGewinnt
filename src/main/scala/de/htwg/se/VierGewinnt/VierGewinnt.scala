4d96ca4cf6d34dafb2ef84da2cc17d17d9f48902
dinhth <thu-ha-dinh@t-online.de>
2021-10-15 11:02:27 +0200

    added Player names

--- src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
@@ -1,7 +1,14 @@
 package de.htwg.se.VierGewinnt
 
+import scala.io.StdIn.readLine
+
 object VierGewinnt {
   def main(args: Array[String]): Unit = {
+    var input:String=""
+    input = readLine()
+    println("1.Spieler: " + input)
+    input = readLine()
+    println("2.Spieler: " + input)
     println("Vier Gewinnt")
   }
 

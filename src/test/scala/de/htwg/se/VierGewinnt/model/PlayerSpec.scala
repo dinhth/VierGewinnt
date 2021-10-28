cb4b1096fa97c04d6e86b7906e9417a33ba4e9a1
dinhth <thu-ha-dinh@t-online.de>
2021-10-28 22:05:12 +0200

    added test

--- src/test/scala/de/htwg/se/VierGewinnt/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/model/PlayerSpec.scala
@@ -4,6 +4,9 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class PlayerSpec extends AnyWordSpec {
-
-
+  "A Player 1" should {
+    "have a String representation" in {
+      Player("player1").toString should be("player1")
+    }
+  }
 }
--- src/test/scala/de/htwg/se/VierGewinnt/model/PlaygroundSpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/model/PlaygroundSpec.scala
@@ -4,5 +4,10 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class PlaygroundSpec extends AnyWordSpec {
-
+  "VierGewinnt" should {
+    "have a String representation" in {
+      val matrix = Playground(7)
+      matrix.toString should be("")
+    }
+  }
 }

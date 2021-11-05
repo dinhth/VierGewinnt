0e96c1d00a12086dcb3145bf0370dec294e640b6
Orkan23 <orkan2315@gmail.com>
2021-11-05 11:10:38 +0100

    rewrite tests for Playground.scala

--- src/test/scala/de/htwg/se/VierGewinnt/model/PlaygroundSpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/model/PlaygroundSpec.scala
@@ -7,27 +7,32 @@ import io.AnsiColor.*
 import scala.math.ceil
 
 class PlaygroundSpec extends AnyWordSpec {
-  "Playground is the string representation of the gamefield" when {
-    "to be constructed" should {
-      val p0 = new Playground(10)
-      "have a custom size" in {
-        p0.size should be(10)
-      }
+  "Playground is the representation of the gamefield" when {
+    "initialized without parameter" should {
       val playground = new Playground()
-      "have the right default size" in {
+      val player = new Player("Player 1", Chip.RED)
+      "have the default size" in {
         playground.size should be(7)
       }
+      val playgroundcpy = playground.insertChip(0, player)
+      "Player should insert a chip in a choosen column" in {
+        playgroundcpy.grid.getCell(playgroundcpy.size - 1, 0).chip should be(Chip.RED)
+      }
+      "request how many chips inserted in a column" in {
+        playgroundcpy.getPosition(0) should be(playgroundcpy.size - 2)
+      }
+
+    }
+    "have a String representation with a head, grid and border" should {
+      val playground = new Playground(7)
       "have a line with column names" in {
         playground.colnames() should be(s"${BLUE_B}\t1\t2\t3\t4\t5\t6\t7\t ${RESET}\n")
       }
       "have a bottom border" in {
         playground.border() should be(s"${BLUE_B}  -----------------------------  ${RESET}\n")
       }
-      "should print the right representation with an example line" in {
-        playground.toString should be(playground.colnames() + playground.grid + s"${BLUE_B}  " +
-          (s"${BLUE_B}|${YELLOW_B}   ${RESET}") * (playground.size.toFloat / 2).toInt +
-          (s"${BLUE_B}|${RED_B}   ${RESET}") * ceil(playground.size.toFloat / 2).toInt +
-          s"${BLUE_B}|  ${RESET}\n" + playground.border())
+      "should print the whole gamefield" in {
+        playground.toString should be(playground.colnames() + playground.grid + playground.border())
       }
     }
   }

781f6429145f0e40fa6c188df0991918e7df00f8
Orkan23 <orkan2315@gmail.com>
2021-11-05 10:10:31 +0100

    tests for chip

--- src/test/scala/de/htwg/se/VierGewinnt/model/ChipSpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/model/ChipSpec.scala
@@ -2,7 +2,19 @@ package de.htwg.se.VierGewinnt.model
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
+import scala.io.AnsiColor._
 
 class ChipSpec extends AnyWordSpec {
+
+  "create chip for empty cell" when {
+    val chip = Chip.EMPTY
+    "have value 0" in {
+      chip.getValue should be(0)
+    }
+    "have color Blue" in {
+      chip.getColorCode should be(BLUE_B)
+    }
+  }
+
 
 }

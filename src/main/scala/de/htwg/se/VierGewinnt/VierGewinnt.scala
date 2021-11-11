ed80b59794563767726cc2b6da3b80534d258a2c
dinhth <thu-ha-dinh@t-online.de>
2021-11-11 01:05:01 +0100

    changed while loop to match case

--- src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
@@ -1,11 +1,19 @@
 package de.htwg.se.VierGewinnt
 
-import de.htwg.se.VierGewinnt.aview.Tui
-import de.htwg.se.VierGewinnt.model.{Cell, Chip, Grid, Player, Playground}
-
+import aview.Tui
+import controller.Controller
+import model.{Cell, Chip, Grid, Player, Playground}
 import scala.io.StdIn.readLine
 
+/*
+@main def run: Unit =
+  val playground = new Playground(size = 7)
+  val controller = Controller(playground)
+  val tui = Tui(controller)
+  tui.run
+*/
 
+/*
 object VierGewinnt {
   var playground = new Playground(size = 7)
   val tui = new Tui
@@ -32,3 +40,35 @@ object VierGewinnt {
   }
 
 }
+*/
+
+//Match Version
+  val player = List(Player("Player 1", Chip.YELLOW), Player("Player 2", Chip.RED))
+  var i = 0
+
+@main def run: Unit =
+  val playground = new Playground(size = 7)
+
+  println("Welcome to 'Vier Gewinnt' " + player(0).name + " and " + player(1).name)
+  println(playground.toString)
+  getInputAndPrintLoop(playground: Playground)
+
+def getInputAndPrintLoop(playground: Playground): Unit =
+  println(s"It's your turn ${player(i % 2)}")
+  val input = readLine
+  val chars = input.toCharArray
+  input match {
+    case "q" =>
+
+    case _ => {
+      val in = chars(0).toString.toInt
+      val p = player(i % 2)
+
+      val newplayground = playground.insertChip(in - 1, p)
+      i += 1
+      println(newplayground)
+      getInputAndPrintLoop(newplayground)
+    }
+  }
+
+

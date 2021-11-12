0cea740760b356a792fa14d634dca09393ed6fea
Orkan23 <orkan2315@gmail.com>
2021-11-12 07:18:56 +0100

    Constructing MVC - TUI is only checking valid or invalid inputs. - instead of the TUI, Playground now checks if a column is already full. Otherwise TUI would have access to model - now Player is an attribute of Playground as well

--- src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
@@ -5,70 +5,14 @@ import controller.Controller
 import model.{Cell, Chip, Grid, Player, Playground}
 import scala.io.StdIn.readLine
 
-/*
+
 @main def run: Unit =
-  val playground = new Playground(size = 7)
-  val controller = Controller(playground)
+  val controller = new Controller()
   val tui = Tui(controller)
   tui.run
-*/
-
-/*
-object VierGewinnt {
-  var playground = new Playground(size = 7)
-  val tui = new Tui
-
-  def main(args: Array[String]): Unit = {
-    val player = List(Player("Player 1", Chip.YELLOW), Player("Player 2", Chip.RED))
-    println("Welcome to 'Vier Gewinnt' " + player(0).name + " and " + player(1).name)
-
-    var input = ""
-    var i = 0
-    println(playground)
 
 
-    while {
-      println(s"It's your turn ${player(i % 2)}")
-      input = readLine();
-      playground = tui.evaluate(input, player(i % 2), playground)
-      println(playground)
-      i += 1;
-      input != "q"
-    }
-    do ()
 
-  }
 
-}
-*/
-
-//Match Version
-  val player = List(Player("Player 1", Chip.YELLOW), Player("Player 2", Chip.RED))
-  var i = 0
-
-@main def run: Unit =
-  val playground = new Playground(size = 7)
-
-  println("Welcome to 'Vier Gewinnt' " + player(0).name + " and " + player(1).name)
-  println(playground.toString)
-  getInputAndPrintLoop(playground: Playground)
-
-def getInputAndPrintLoop(playground: Playground): Unit =
-  println(s"It's your turn ${player(i % 2)}")
-  val input = readLine
-  val chars = input.toCharArray
-  input match {
-    case "q" =>
-
-    case _ => {
-      val in = chars(0).toString.toInt
-      val p = player(i % 2)
-
-      val newplayground = playground.insertChip(in - 1, p)
-      i += 1
-      println(newplayground)
-      getInputAndPrintLoop(newplayground)
-    }
-  }
 
 
--- src/main/scala/de/htwg/se/VierGewinnt/aview/Tui.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/aview/Tui.scala
@@ -9,51 +9,31 @@ import util.Observer
 import scala.util.Try
 
 
-class Tui() {
-  def evaluate(input: String, player: Player, playground: Playground): Playground = {
-    var in = input
-    while (in.toIntOption == None || in.toInt < 1 || in.toInt > playground.size || playground.getPosition(in.toInt - 1) == -1)
-      println("falsche Eingabe")
-      in = readLine()
-    println(player)
-    playground.insertChip(in.toInt - 1, player)
-  }
-}
-
-
-/*
-class Tui(controller: Controller) extends Observer:
+class Tui(controller: Controller) extends Observer :
   controller.add(this)
-  
-  val playground = new Playground(size = 7)
-  val player = List(Player("Player 1", Chip.YELLOW), Player("Player 2", Chip.RED))
-  var i = 0
+  val size = 7
 
   def run =
-    println("Welcome to 'Vier Gewinnt' " + player(0).name + " and " + player(1).name)
-    println(playground.toString)
+    println("Welcome to 'Vier Gewinnt' ")
+    println(controller.toString)
     getInputAndPrintLoop()
 
-  override def update: Unit = ???
-
   def getInputAndPrintLoop(): Unit =
-    println(s"It's your turn ${player(i % 2)}")
-  val input = readLine
-  val chars = input.toCharArray
-  input match {
-    case "q" =>
-
-    case _ => {
-      val in = chars(0).toString.toInt
-      val p = player(i % 2)
-
-      val newplayground = playground.insertChip(in - 1, p)
-      i += 1
-      println(newplayground)
-      getInputAndPrintLoop()
+    val input = readLine
+    val chars = input.toCharArray
+    input match {
+      case x if x.toIntOption == None =>
+        println("doesn't look like a number")
+        getInputAndPrintLoop()
+      case x if x.toInt < 1 || x.toInt > size =>
+        println("wrong input, try a number from 1 to " + size)
+        getInputAndPrintLoop()
+      case _ =>
+        controller.insertChip(input.toInt - 1)
+        getInputAndPrintLoop()
     }
-  }
-*/    
+
+  override def update: Unit = println(controller.toString)
 
 
 

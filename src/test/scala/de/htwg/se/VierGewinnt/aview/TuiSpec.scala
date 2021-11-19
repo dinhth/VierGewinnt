8e20233732a166cdf1dbed22565ad9453962aedf
Orkan23 <orkan2315@gmail.com>
2021-11-19 04:31:14 +0100

    rewrite TUI tests with custom Print/Input-Streams

--- src/test/scala/de/htwg/se/VierGewinnt/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/aview/TuiSpec.scala
@@ -4,15 +4,59 @@ import de.htwg.se.VierGewinnt.controller.Controller
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers.*
 
+import java.io.{BufferedReader, ByteArrayInputStream, ByteArrayOutputStream, PrintStream, StringReader}
+
 class TuiSpec extends AnyWordSpec {
   "TUI of VierGewinnt" should {
     val controller = new Controller()
     val tui = new Tui(controller)
 
+    "run" in {
+      val in = new BufferedReader(new StringReader("q"))
+      val source = new ByteArrayOutputStream()
+      val printer = new PrintStream(source)
+      Console.withOut(printer) {
+        Console.withIn(in) {
+          tui.run
+        }
+      }
+      source.toString should be(s"Welcome to 'Vier Gewinnt'\n$controller\n")
+    }
+
     "valid input" in {
-      controller.insertChip(0)
+      val in = new BufferedReader(new StringReader("1\nq\n"))
+      val source = new ByteArrayOutputStream()
+      val printer = new PrintStream(source)
+      Console.withOut(printer) {
+        Console.withIn(in) {
+          tui.getInputAndPrintLoop()
+        }
+      }
       tui.size should be(7)
       controller.playground.getPosition(0) should be(5)
+    }
+    "invalid input: not a number" in {
+      val in = new BufferedReader(new StringReader("a\n \n+\n\nq\n"))
+      val source = new ByteArrayOutputStream()
+      val printer = new PrintStream(source)
+      Console.withOut(printer) {
+        Console.withIn(in) {
+          tui.getInputAndPrintLoop()
+        }
+      }
+
+      source.toString should be("doesn't look like a number\n" * 4)
+    }
+    "invalid input: a number but not in range" in {
+      val in = new BufferedReader(new StringReader("0\n8\n-1\nq\n"))
+      val source = new ByteArrayOutputStream()
+      val printer = new PrintStream(source)
+      Console.withOut(printer) {
+        Console.withIn(in) {
+          tui.getInputAndPrintLoop()
+        }
+      }
+      source.toString should be(("wrong input, try a number from 1 to " + tui.size + "\n") * 3)
     }
   }
 }

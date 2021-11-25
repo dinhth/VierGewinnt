a5b020f06c483a7610f990b1c9607771de56572e
dinhth <thu-ha-dinh@t-online.de>
2021-11-25 20:31:36 +0100

    added methods for win check and strategy pattern

--- src/main/scala/de/htwg/se/VierGewinnt/controller/Controller.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/controller/Controller.scala
@@ -1,15 +1,31 @@
 package de.htwg.se.VierGewinnt
 package controller
 
-import model.{Chip, Player, Playground}
+import model.{Chip, Grid, Player, Playground}
 import util.Observable
 
 class Controller(var playground: Playground) extends Observable :
   def this(size: Int = 7) = this(new Playground(7))
 
-  def insertChip(col: Int):Unit =    
+  def insertChip(col: Int):Unit =
     playground = playground.insertChip(col)
+    checkFull()
+    checkWinner()
     notifyObservers
 
-  override def toString = playground.toString
+  def checkFull():Unit =
+    playground.grid.checkFull() match { //println just FOR DEBUG, DELETE LATER!!! (print ln ONLY in TUI)
+      case true => println("Grid is COMPLETELY FULL")
+      case false => println("Grid is not full")
+    }
 
+  def changeEnemyStrategy(strat:String):Unit =
+    playground = playground.setEnemyStrategy(strat)
+
+  def checkWinner():Unit =
+    playground.grid.checkWin() match {//println just FOR DEBUG, DELETE LATER!!! (print ln ONLY in TUI)
+      case 0 =>
+      case 1 => println("Winner is Red")
+      case 2 => println("Winner is Yellow")
+    }
+  override def toString = playground.toString

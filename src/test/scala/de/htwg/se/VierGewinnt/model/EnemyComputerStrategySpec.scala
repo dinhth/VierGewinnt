81c6b64bcd84f4537fddad14a9db7647e8edf990
dinhth <thu-ha-dinh@t-online.de>
2021-11-25 23:29:52 +0100

    added tests for strategy pattern

--- src/test/scala/de/htwg/se/VierGewinnt/model/EnemyComputerStrategySpec.scala
+++ src/test/scala/de/htwg/se/VierGewinnt/model/EnemyComputerStrategySpec.scala
@@ -13,5 +13,21 @@ class EnemyComputerStrategySpec extends AnyWordSpec {
       val tempplayground = playground.setEnemyStrategy("computer")
       tempplayground.insertChip(1) should not be (Playground(Grid(Vector.tabulate(7, 7)((row, col) => Cell(Chip.EMPTY))).replaceCell(6, 1, Cell(Chip.YELLOW)), List(Player("Player 2", Chip.RED), Player("Player 1", Chip.YELLOW)), EnemyComputerStrategy()))
     }
+    "not inserting a Chip manually" in {
+      var tempplayground = playground.setEnemyStrategy("computer")
+      for (i <- 0 to 8) {
+        tempplayground = tempplayground.insertChip(1)
+      }
+      tempplayground.insertChip(1) should be (tempplayground)
+    }
+    "not inserting a Chip by computer" in {
+      var tempplayground = playground.setEnemyStrategy("person")
+      for (i <- 1 to 7) yield {
+        for (j <- 0 to 6) yield {
+          tempplayground = tempplayground.insertChip(j)
+        }
+      }
+      tempplayground.insertChip(1) should be (tempplayground)
+    }
   }
 }

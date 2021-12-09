7d87ac5b2998bea2ecbd02e3b10a5f150ddb5563
dinhth <thu-ha-dinh@t-online.de>
2021-12-10 00:36:52 +0100

    minor change

--- src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/VierGewinnt.scala
@@ -8,8 +8,8 @@ import scala.io.StdIn.readLine
 
 @main def run: Unit =
   val controller = new Controller()
-  val gui = GUI(controller)
-  gui.run
+  //val gui = GUI(controller)
+  //gui.run
   val tui = Tui(controller)
   tui.run
 

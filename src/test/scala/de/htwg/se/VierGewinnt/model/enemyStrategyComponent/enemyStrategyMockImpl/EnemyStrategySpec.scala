fa8b77f31eaacec38f9fd332cc22bccb4a78bfc1
dinhth <thu-ha-dinh@t-online.de>
2022-01-14 10:19:25 +0100

    added test files

---
+++ src/test/scala/de/htwg/se/VierGewinnt/model/enemyStrategyComponent/enemyStrategyMockImpl/EnemyStrategySpec.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.VierGewinnt.model.enemyStrategyComponent.enemyStrategyMockImpl
+
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+
+class EnemyStrategySpec extends AnyWordSpec {
+
+}
---
+++ src/test/scala/de/htwg/se/VierGewinnt/model/gridComponent/gridMockImpl/GridSpec.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.VierGewinnt.model.gridComponent.gridMockImpl
+
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.wordspec.AnyWordSpec
+
+class GridSpec extends AnyWordSpec {
+
+}

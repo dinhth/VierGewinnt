2a3243034cb3c650fe83085dd61c4fcb98ce53e1
dinhth <thu-ha-dinh@t-online.de>
2022-01-23 03:32:50 +0100

    deleted unnecessary comments

--- src/main/scala/de/htwg/se/VierGewinnt/model/playerComponent/PlayerInterface.scala
+++ src/main/scala/de/htwg/se/VierGewinnt/model/playerComponent/PlayerInterface.scala
@@ -6,5 +6,3 @@ import de.htwg.se.VierGewinnt.model.playerComponent.playerBaseImpl.{BotPlayer, H
 trait PlayerInterface:
   def getChip():Chip
   def getName():String
-  //def getHumanPlayer(name: String, chip: Chip): PlayerInterface = new HumanPlayer(name, chip)
-  //def getBotPlayer(name: String, chip: Chip): PlayerInterface = new BotPlayer(name, chip)

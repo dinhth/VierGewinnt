c36c1830de95d868ec55604be5f64bbd64f88dac
dinhth <thu-ha-dinh@t-online.de>
2022-01-23 03:31:48 +0100

    deleted unnecessary files

--- src/test/scala/de/htwg/se/VierGewinnt/controller/controllerComponent/ControllerInterfaceSpec.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.VierGewinnt.controller.controllerComponent
-
-import org.scalatest.matchers.should.Matchers.*
-import org.scalatest.wordspec.AnyWordSpec
-
-class ControllerInterfaceSpec extends AnyWordSpec{
-
-}

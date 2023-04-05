6a0f086c6ca33d47a9c037692a69f45716cea03a
Orkan23 <orkan2315@gmail.com>
2023-04-05 03:23:27 +0200

    test: add test for FileIO/Util (#5)
    add test for FileIO/Util

---
+++ src/test/scala/de/htwg/se/VierGewinnt/model/fileIoComponent/UtilSpec.scala
@@ -0,0 +1,24 @@
+package de.htwg.se.VierGewinnt.model.fileIoComponent
+
+import de.htwg.se.VierGewinnt.model.fileIoComponent.Util.getNameAndChip
+import de.htwg.se.VierGewinnt.model.gridComponent.gridBaseImpl.Chip
+import org.scalatest.matchers.dsl.MatcherWords.be
+import org.scalatest.matchers.should.Matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+
+class UtilSpec extends AnyWordSpec {
+  "The Util singleton" when {
+    "called getNameAndChip function" should {
+      val resultYellow = getNameAndChip("Player 1&YELLOW")
+      val resultRed = getNameAndChip("Player 2&RED")
+
+      "return a tuple name and chip of the player" in {
+        resultYellow._1 should be("Player 1")
+        resultYellow._2 should be(Chip.YELLOW)
+
+        resultRed._1 should be("Player 2")
+        resultRed._2 should be(Chip.RED)
+      }
+    }
+  }
+}
